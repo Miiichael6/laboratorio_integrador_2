2bf5e4d839b3454e29f6aa0e13c21e9c63eae399
Miiichael6 <cdlsmichael.16@gmail.com>
2025-11-26 23:36:52 -0500

    cambios

--- txt.py
+++ txt.py
@@ -2,3 +2,11 @@
 # EJECUTAR CON python -m tienda_electronicos.main
 
 # C:\Users\USER\Desktop\Michael\11_tecsup\1_semestre\6_fundamentos_en_gestion_de_datos\03_grupal\lab_02_2
+
+
+# eliminar pychcahe
+
+# git rm -r --cached __pycache__
+# git rm --cached -r *.pyc
+# git commit -m "Remove pycache from tracking"
+# git push
