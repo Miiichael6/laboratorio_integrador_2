3798386247103d0c88a121463894ea360b43839a
Miiichael6 <cdlsmichael.16@gmail.com>
2025-12-09 18:38:31 -0500

    feat: Add `DatabaseGestor` class for SQLite database connection and CRUD operations.

--- tienda_electronicos/base_datos/database_gestor.py
+++ tienda_electronicos/base_datos/database_gestor.py
@@ -8,7 +8,7 @@ class DatabaseGestor:
 
     def connect(self):
         conn = sqlite3.connect(self.db_path)
-        conn.row_factory = sqlite3.Row  # <- la clave para obtener dicts
+        conn.row_factory = sqlite3.Row 
         return conn
 
     def execute(self, query, params=()):
