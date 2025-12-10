6f48e21bd3ff39274bc8b4a8ddb1c707bcbaa5ea
Elvis <egomez083@gmail.com>
2025-12-09 20:46:22 -0500

    cambios

--- txt.py
+++ txt.py
@@ -1,5 +1,5 @@
 
-# EJECUTAR CON python -m tienda_electronicos.main
+# EJECUTAR CON      python -m tienda_electronicos.main
 
 # C:\Users\USER\Desktop\Michael\11_tecsup\1_semestre\6_fundamentos_en_gestion_de_datos\03_grupal\lab_02_2
 
